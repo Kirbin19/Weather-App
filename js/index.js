0a537c5d808e95eb7358c9f6bf0a0fb2997e29cd
Kirbin <k9881572990@gmail.com>
2025-09-30 01:12:23 +0300

    1.1

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const apiKey = "cc723af2decb64a729cd1147c926944a";
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector(".search-button");
+
+searchButton.addEventListener("click", () => {
+  getWeather(searchInput.value);
+  searchInput.value = "";
+});
+
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === 'Enter') {
+    getWeather(searchInput.value);
+    searchInput.value = "";
+  }
+});
+
+async function getWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+  const info = await response.json();
+  document.querySelector(".weather-city").innerHTML = info.name;
+  document.querySelector(".weather-temp").innerHTML =
+    Math.round(info.main.temp) + "&deg;C";
+  document.querySelector(".humidity-value").innerHTML =
+    info.main.humidity + "%";
+  document.querySelector(".wind-value").innerHTML =
+    Math.round(info.wind.speed) + " Км/ч";
+
+  console.log(info);
+}
